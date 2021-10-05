43a7bc312035ffd204a4efd2696d4bb09a5fecaf
Chanhyuk Park <pgg6713@gmail.com>
2021-10-06 02:50:33 +0900

    finish tinder clone only front

--- TinderClone/App.js
+++ TinderClone/App.js
@@ -1,13 +1,19 @@
 import 'react-native-gesture-handler';
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, SafeAreaView, Text, Pressable} from 'react-native';
 import Card from './src/components/TinderCard';
 import users from './assets/data/users';
-
 import AnimatedStack from './src/components/AnimatedStack';
 import MatchesScreen from "./src/screens/MatchesScreen";
+import Fontisto from "react-native-vector-icons/Fontisto";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import HomeScreen from "./src/screens/HomeScreen";
+
 
 const App = () => {
+    const [activeScreen, setActiveScreen] = useState('HOME');
     const onSwipeLeft = user => {
         console.warn('swipe left', user.name);
     };
@@ -15,26 +21,59 @@ const App = () => {
     const onSwipeRight = user => {
         console.warn('swipe right: ', user.name);
     };
-
+    const color = '#b5b5b5';
+    const activeColor = '#F76C6B';
     return (
-        <View style={styles.pageContainer}>
-            {/*<AnimatedStack*/}
-            {/*    data={users}*/}
-            {/*    renderItem={({item}) => <Card user={item} />}*/}
-            {/*    onSwipeLeft={onSwipeLeft}*/}
-            {/*    onSwipeRight={onSwipeRight}*/}
-            {/*/>*/}
+        <SafeAreaView style={styles.root}>
+            <View style={styles.pageContainer}>
+                <View style={styles.topNavigation}>
+                    <Pressable onPress={() => setActiveScreen('HOME')} >
+                        <Fontisto
+                            name="tinder"
+                            size={30}
+                            color={activeScreen === 'HOME' ? activeColor : color}
+                        />
+                    </Pressable>
+                    <MaterialCommunityIcons
+                        name="star-four-points"
+                        size={30}
+                        color={color}
+                    />
+                    <Pressable onPress={() => setActiveScreen('CHAT')}>
+                        <Ionicons
+                            name="ios-chatbubbles"
+                            size={30}
+                            color={activeScreen === 'CHAT' ? activeColor : color}
+                        />
+                    </Pressable>
+                    <FontAwesome
+                        name="user"
+                        size={30}
+                        color={activeScreen === 'PROFILE' ? activeColor : color}
+                    />
+                </View>
+                {activeScreen === 'HOME' && <HomeScreen />}
+                {activeScreen === 'CHAT' && <MatchesScreen />}
 
-            <MatchesScreen/>
-        </View>
+            </View>
+        </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
+    root: {
+        flex: 1,
+    },
     pageContainer: {
         justifyContent: 'center',
         alignItems: 'center',
         flex: 1,
+    },
+    topNavigation: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: '100%',
+        padding: 10,
     },
 });
 
