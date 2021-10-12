bd022bf18710e86118aeca2d7e2203921ff8782d
Chanhyuk Park <pgg6713@gmail.com>
2021-10-12 17:24:08 +0900

    youtube clone ui complete

--- youtubeClone/screens/NotFoundScreen.tsx
+++ youtubeClone/screens/NotFoundScreen.tsx
@@ -1,9 +1,12 @@
+import { StackScreenProps } from '@react-navigation/stack';
 import * as React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-import { RootStackScreenProps } from '../types';
+import { RootStackParamList } from '../types';
 
-export default function NotFoundScreen({ navigation }: RootStackScreenProps<'NotFound'>) {
+export default function NotFoundScreen({
+  navigation,
+}: StackScreenProps<RootStackParamList, 'NotFound'>) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>This screen doesn't exist.</Text>
